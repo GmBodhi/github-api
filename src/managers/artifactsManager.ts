7ad4d3be2937baedc5eca24719091ab1031f0210
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-07-02 15:35:59 +0530

    fix(artifactmanager): minor fix and renamed action.ts to actions.ts

--- src/managers/artifactsManager.ts
+++ src/managers/artifactsManager.ts
@@ -3,51 +3,55 @@ import Client from "../structures/client";
 import Manager from "./manager";
 
 class ArtifactsManager extends Manager {
-    owner: string;
-    repo: string;
-    constructor({ client }: { client: Client }, {owner, repo}: {owner: string, repo: string}){
-        super({ client })
-        this.owner = owner;
-        this.repo = repo;
-    }
-    async list(options: {
-        page?: number
-        per_page?: number
-    }) {
-       return await this.client.api
-        .req(`/repos/${this.owner}/${this.repo}/actions/artifacts`, {query: options})
-        .get()
-        .then(async (res: Response) =>  {
-            return await res.json()
-        })
-        .catch((e: any) => {
-            throw new Error(e);
-        })
-    }
-    async get(artifactId: number) {
-        return await this.client.api
-        .req(`/repos/${this.owner}/${this.repo}/actions/artifacts/${artifactId}`)
-        .get()
-        .then(async (res: Response) => {
-            return await res.json()
-        })
-    }
-    async delete(artifactId: number){
-        return await this.client.api
-        .req(`/repos/${this.owner}/${this.repo}/actions/artifacts/${artifactId}`)
-        .delete()
-        .then(async (res: Response) => {
-            return await res.json()
-        })
-    }
-    async download(artifactId: number){
-        return await this.client.api
-        .req(`/repos/${this.owner}/${this.repo}/actions/artifacts/${artifactId}/zip`)
-        .get()
-        .then(async (res: Response) => {
-            return await res.json()
-        })
-    }
+  owner: string;
+  repo: string;
+  constructor(
+    { client }: { client: Client },
+    { owner, repo }: { owner: string; repo: string }
+  ) {
+    super({ client });
+    this.owner = owner;
+    this.repo = repo;
+  }
+  async list(options: { page?: number; perPage?: number }) {
+    return await this.client.api
+      .req(`/repos/${this.owner}/${this.repo}/actions/artifacts`, {
+        query: options,
+      })
+      .get()
+      .then((res: Response) => {
+        return res.json();
+      })
+      .catch((e: any) => {
+        throw new Error(e);
+      });
+  }
+  async get(artifactId: number) {
+    return await this.client.api
+      .req(`/repos/${this.owner}/${this.repo}/actions/artifacts/${artifactId}`)
+      .get()
+      .then((res: Response) => {
+        return res.json();
+      });
+  }
+  async delete(artifactId: number) {
+    return await this.client.api
+      .req(`/repos/${this.owner}/${this.repo}/actions/artifacts/${artifactId}`)
+      .delete()
+      .then((res: Response) => {
+        return res.json();
+      });
+  }
+  async download(artifactId: number) {
+    return await this.client.api
+      .req(
+        `/repos/${this.owner}/${this.repo}/actions/artifacts/${artifactId}/zip`
+      )
+      .get()
+      .then((res: Response) => {
+        return res.json();
+      });
+  }
 }
 
 export default ArtifactsManager;
