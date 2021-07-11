13895ed79e1fcd70ef7af98935a07176703bb76d
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-07-11 12:00:56 +0530

    feat(test): minor

--- tests/client.test.ts
+++ tests/client.test.ts
@@ -13,22 +13,17 @@ const getRandom = () =>
 
 describe("Login client and fetch a user", function () {
   this.timeout(5000);
+  it("fetch gmbodhi", (done) => {
+    client.users.fetch("gmbodhi").then((user: any) => {
+      assert(typeof user !== "undefined", "got one user");
+      done();
+    });
+  });
   it("login the client", (done) => {
     client.on("ready", () => {
       expect(client.user, "user not null").not.to.be.equal(null);
       done();
-      describe("changing user's profile details", function () {
-        this.timeout(5000);
-        it("change user's name to a random value", (done) => {
-          client.user.setName(getRandom()).then(() => done());
-        });
-        it("change user's bio to a random value", (done) => {
-          client.user.setBio(getRandom()).then(() => done());
-        });
-        it("change user's location to a random value", (done) => {
-          client.user.setLocation(getRandom()).then(() => done());
-        });
-      });
+      console.log(`Logged in as ${client.user.login}`);
     });
   });
 });
