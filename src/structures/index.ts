537c52e811e475b79bcb17fecc3d5b28230d404a
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-07-12 20:27:12 +0530

    feat: minor

--- src/structures/index.ts
+++ src/structures/index.ts
@@ -1,3 +1,25 @@
 import Client from "./client";
+import Actions from "./actions";
+import Base from "./base";
+import Blocks from "./blocks";
+import ClientUser from "./clientuser";
+import Emails from "./emails";
+import Follows from "./follows";
+import Gist from "./gist";
+import Issue from "./issue";
+import Repo from "./repo";
+import User from "./user";
 
-export { Client };
+export {
+  Client,
+  Actions,
+  Base,
+  Blocks,
+  ClientUser,
+  Emails,
+  Follows,
+  Gist,
+  Issue,
+  Repo,
+  User,
+};
