2221899de76331ba9da545f6a3629e05df8e167f
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-08-05 12:12:07 +0530

    feat: test004

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Docs from "./components/Docs";
 import Home from "./components/Home";
+import "./css/global.css";
 
 function App() {
   return (
