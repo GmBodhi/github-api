ccf9a0b6c446efc8c1ee7602b46def6ce4898894
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-08-10 11:22:13 +0530

    feat: minor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
-import '../css/footer.css';
-import React from 'react';
+import "../css/footer.css";
+import React from "react";
 
 export function Footer() {
-  return <></>;
+  return <>Hmm</>;
 }
