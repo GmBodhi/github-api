65886403229656af53bf06076b76fd44f13d2f0c
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-09-23 12:42:53 +0530

    redirects

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/main.js",
@@ -40,6 +41,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
+    }),
+new CopyPlugin({
+      patterns: [
+        { from: "assets" },
+      ]
     }),
   ],
 };
