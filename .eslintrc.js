d34fcb73a48fc911920181bb4e05c3b5e14e6981
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-09-23 12:14:07 +0530

    fix: rm styling from eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "eslint:recommended", "google"],
+  extends: ["plugin:react/recommended", "eslint:recommended"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
